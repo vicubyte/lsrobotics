2f768206f9e301b580c66e55b7d8ad7d598e8015
vicubyte <vicubyte@gmail.com>
2025-08-11 22:43:26 -0500

    updated texts in lv1 cpi

--- cyberpi/cyberpi_1.js
+++ cyberpi/cyberpi_1.js
@@ -1,119 +1,115 @@
 const slides = [
     {   //Slide 1, 12x18 white box transparent
         image: "srcs/cyberpi/img1/img1_1.png",
-        text: `Thasdasdasdtrong>mBot2</strong>, an educational robot!<br>
-        What is a robot? Do you know other robots?`,
+        text: `Learn how to create a <strong>random answer game</strong> with CyberPi.`,
         className: "anim-shake"
     },
     {   //Slide 2, 20x10 video board
         image: "srcs/cyberpi/img1/img1_2.png",
-        text: `Let’s review the principal components of mBot2.<br>
-        How many of them do you recognize?`,
+        text: `A Magic 8 Ball is a toy that gives random answers to yes-or-no questions.`,
         className: ""
     },
     {   //Slide 3
         image: "srcs/cyberpi/img1/img1_3.png",
-        text: `This is <strong>CyberPi</strong>, the main controller of mBot2. It has all the operation logic.`,
+        text: `How can we make the answers different every time using CyberPi?`,
         className: ""
     },
     {   //Slide 4
         image: "srcs/cyberpi/img1/img1_4.png",
-        text: `These are the <strong>sensors</strong>, they take data from the environment and send it to the controller.`,
+        text: `<strong>Random numbers</strong> make every answer unique!`,
         className: ""
     },
     {   //Slide 5
         image: "srcs/cyberpi/img1/img1_5.png",
-        text: `These are the <strong>actuators</strong>, specifically the motors. They make the mBot move.`,
+        text: `Let’s start coding!<br>
+        Create the opening screen before asking the question.`,
         className: ""
     },
     {   //Slide 6
         image: "srcs/cyberpi/img1/img1_6.png",
-        text: `This is the mBot2 shield, it has a battery and many ports to connect sensors and actuators.`,
+        text: `Click <strong>Bluetooth</strong>, select your CyberPi and pair.<br>
+        You should be in <strong>Upload</strong> mode.`,
         className: ""
     },
     {   //Slide 7
         image: "srcs/cyberpi/img1/img1_7.png",
-        text: `This is the mBot2 chassis, it is made of aluminum. It gives a strong structure to the robot.`,
+        text: `Drag <strong>when button A pressed</strong> block from the Events category and change A to B.`,
         className: ""
     },
     {   //Slide 8
         image: "srcs/cyberpi/img1/img1_8.png",
-        text: `These are the screws, their function is to join parts together. Look at their differences.`,
+        text: `From the Display category, drag the <strong>show label</strong> block and change the text with the opening message.`,
         className: ""
     },
     {   //Slide 9
         image: "srcs/cyberpi/img1/img1_9.png",
-        text: `These are the cables, they connect the actuators and sensors to the mBot2 shield.`,
+        text: `Drag the <strong>when wave left detected</strong> block from Events category and change it to <strong>shaken</strong>.`,
         className: ""
     },
 
     {   //Slide 10
         image: "srcs/cyberpi/img1/img1_10.png",
-        text: `The tires, wheels and mini wheel allows the robot to move freely.`,
+        text: `From the Display category, drag the <strong>show label</strong> block and change the text. Test both events!`,
         className: ""
     },
     {   //Slide 11
         image: "srcs/cyberpi/img1/img1_11.png",
-        text: `To start coding, turn on your mBot2.<br>
-        Remember to turn it off at the end of the class.`,
+        text: `To add sound, drag the <strong>play hi until done</strong> block and change the sound to <strong>magic</strong>.`,
         className: ""
     },
     {   //Slide 12
         image: "srcs/cyberpi/img1/img1_12.png",
-        text: `<a href='https://ide.mblock.cc' target='_blank' rel='noopener noreferrer'>Click here</a> to open mBlock IDE, press <strong>Ctrl+Tab</strong> to return here.<br>
-        What is an <i title="Integrated Development Environment">IDE</i>?`,
+        text: `Make a variable with the name <strong>answer</strong>. This variable will store the random answer.`,
         className: ""
     },
     {   //Slide 13
         image: "srcs/cyberpi/img1/img1_13.png",
-        text: `This is mBlock IDE.<br>
-        Let’s review the principal areas.`,
+        text: `This block stores 0 value in the variable <strong>answer</strong>. We can set different values to this variable throughout the program.`,
         className: ""
     },
     {   //Slide 14
         image: "srcs/cyberpi/img1/img1_14.png",
-        text: `In the Devices area, delete CyberPi and add mBot2.`,
+        text: `This block generates a random number between 1 and 10, which can be used later in the program.`,
         className: ""
     },
     {   //Slide 15
         image: "srcs/cyberpi/img1/img1_15.png",
-        text: `Set mBot2 as mostly used device. Next time mBot2 will appear as default device in mBlock.`,
+        text: `Drag the following blocks under the play block. This will store a random number in the <strong>answer</strong> variable.`,
         className: ""
     },
     {   //Slide 16
         image: "srcs/cyberpi/img1/img1_16.png",
-        text: `Click Bluetooth, select your mBot2 and Pair.<br>
-        You should be in Live mode.`,
+        text: `This block makes the program run an instruction if the condition is true.`,
         className: ""
     },
     {   //Slide 17
         image: "srcs/cyberpi/img1/img1_17.png",
-        text: `Open Setting and activate <strong>Block Area Fixed Pattern</strong>.<br>
-        Now you always see the available blocks.`,
+        text: `This block returns <strong>True</strong> if the variable <strong>answer</strong> is 1, and <strong>False</strong> if it’s not.`,
         className: ""
     },
     {   //Slide 18
         image: "srcs/cyberpi/img1/img1_18.png",
-        text: `Create your mBlock account!<br>
-        Click in panda icon and select Google.`,
+        text: `Drag the following blocks. This script will show <strong>YES</strong> only when the value of the variable <strong>answer</strong> equals 1.`,
         className: ""
     },
     {   //Slide 19
         image: "srcs/cyberpi/img1/img1_19.png",
-        text: `Select your Markham account and continue the process.<br>
-        You will see your profile icon.`,
+        text: `Build the same structure for the other possible values. Place the code blocks one below the other.`,
         className: ""
     },
     {   //Slide 20
         image: "srcs/cyberpi/img1/img1_20.png",
-        text: `Remember to log in to all classes to save your projects to the cloud.<br>
-        Notice the change in the icon.`,
+        text: `Join all the blocks together into one program.`,
         className: ""
     },
     {   //Slide 21
         image: "srcs/cyberpi/img1/img1_21.png",
-        text: `Click <strong>play hi until done</strong> block.<br>
-        What does mBot2 do?`,
+        text: `<strong>Upload</strong> your program and test it by asking some questions!`,
+        className: ""
+    },
+    {   //Slide 22
+        image: "srcs/cyberpi/img1/img1_22.png",
+        text: `<strong>Challenge:</strong> Add more possible answers to your Magic CyberPi!`,
         className: ""
     },
 ];
