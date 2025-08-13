adb02f12b2a185538f3e2354723646f23dce5f62
vicubyte <vicubyte@gmail.com>
2025-08-13 14:07:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeTitle() {
-    document.getElementById("slide-title").style.display = "none";
-    document.getElementById("slide-device").style.display = "flex";
-}
-window.onload = function() {
-    setTimeout(changeTitle, 1000);
-};
