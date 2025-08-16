1be76da215b0502591cdebd08dd1cd92f7d02a41
vicubyte <vicubyte@gmail.com>
2025-08-15 19:30:40 -0500

    CyberPi Class2 Material
    Se subió todo el nuevo material de esa clase

--- cyberpi/cyberpi_2.js
+++ cyberpi/cyberpi_2.js
@@ -1,108 +1,124 @@
 const slides = [
     {   //Slide 1
-        image: "srcs/mbot2/img2/img2_1.png",
-        text: `Learn how to <strong>move</strong> the mBot2 with precision.`,
-        className: "anim-move-up"
-    },
-    {   //Slide 2
-        video: "srcs/mbot2/img2/img2_2.mp4",
-        text: `What kind of movements do you know?<br>
-        Can you give more examples?`,
+        video: "srcs/cyberpi/img2/img2_1.mp4",
+        text: `Learn how to make an <strong>animation</strong> with CyberPi.`,
         className: ""
     },
+    {   //Slide 2
+        image: "srcs/cyberpi/img2/img2_2.png",
+        text: `How is a digital image made?`,
+        className: "anim-shake"
+    },
     {   //Slide 3
-        image: "srcs/mbot2/img2/img2_3.png",
-        text: `What makes mBot2 move?`,
+        image: "srcs/cyberpi/img2/img2_3.png",
+        text: `Pixels are the tiny squares that make up an image.`,
         className: "anim-shake"
     },
     {   //Slide 4
-        image: "srcs/mbot2/img2/img2_4.png",
-        text: `The mBot2 uses two <strong>encoder motors</strong> to move!<br>
-        Where else do you find motors?`,
+        video: "srcs/cyberpi/img2/img2_1.mp4",
+        text: `In a sprite animation, we show different images very quickly, so they look like they are moving.`,
         className: ""
     },
     {   //Slide 5
-        video: "srcs/mbot2/img2/img2_5.mp4",
+        image: "srcs/cyberpi/img2/img2_5.png",
         text: `Let’s start coding!<br>
-        Create a program that moves forward the mBot2.`,
-        className: ""
+        Create a car animation.`,
+        className: "anim-shake"
     },
     {   //Slide 6
-        image: "srcs/mbot2/img2/img2_6.png",
-        text: `Drag the following blocks to the workspace.<br>
-        What happens when you press the Button A in mBot2?`,
+        image: "srcs/cyberpi/img2/img2_6.png",
+        text: `First, we need a new extension.<br>
+        Click <strong>extension</strong>, and add the <strong>Display+</strong>.`,
         className: ""
     },
     {   //Slide 7
-        image: "srcs/mbot2/img2/img2_7.png",
-        text: `Let’s modify the <strong>speed</strong>!<br>
-        What happens when you change RPM to 30?<br>
-        What does <i title="Revolutions Per Minute">RPM</i> mean?`,
+        image: "srcs/cyberpi/img2/img2_7.png",
+        text: `Make a variable named <strong>car</strong>. This variable will store our drawing.`,
         className: ""
     },
     {   //Slide 8
-        video: "srcs/mbot2/img2/img2_8.mp4",
-        text: `At same time, more speed is more distance!`,
+        image: "srcs/cyberpi/img2/img2_8.png",
+        text: `Drag the following blocks, the purple one is from <strong>Sprites</strong> category.`,
         className: ""
     },
     {   //Slide 9
-        image: "srcs/mbot2/img2/img2_9.png",
-        text: `Let’s modify the <strong>time</strong>!<br>
-        What happens when you change secs to 2?`,
+        image: "srcs/cyberpi/img2/img2_9.png",
+        text: `Click the <strong>M icon</strong> to open the editor window.<br>
+        Then, click the <strong>trash</strong> icon to erase the default drawing.`,
         className: ""
     },
     {   //Slide 10
-        video: "srcs/mbot2/img2/img2_10.mp4",
-        text: `At same speed, more time is more distance!`,
+        image: "srcs/cyberpi/img2/img2_10.png",
+        text: `Draw a car!<br>
+        When you are done, click <strong>OK</strong>.`,
         className: ""
     },
     {   //Slide 11
-        video: "srcs/mbot2/img2/img2_11.mp4",
-        text: `Can mBot2 return to the starting point?`,
+        image: "srcs/cyberpi/img2/img2_11.png",
+        text: `Drag the car variable into the <strong>set sprite</strong> block.`,
         className: ""
     },
     {   //Slide 12
-        image: "srcs/mbot2/img2/img2_12.png",
-        text: `Add a new block and change to moves backward.<br>
-        Use the same speed and time.`,
+        image: "srcs/cyberpi/img2/img2_12.png",
+        text: `Always finish with a <strong>force rendering</strong> block to show the sprite on the screen.<br>
+        The <strong>upload</strong> the promgram and try it!`,
         className: ""
     },
     {   //Slide 13
-        image: "srcs/mbot2/img2/img2_1.png",
-        text: `Create a program that makes mBot2 turn right.`,
-        className: "anim-rotate"
+        image: "srcs/cyberpi/img2/img2_13.png",
+        text: `Let's add a <strong>backdrop</strong>. Add the block, click the color, and change it.<br>
+        Test it on the CyberPi!`,
+        className: ""
     },
     {   //Slide 14
-        image: "srcs/mbot2/img2/img2_14.png",
-        text: `Drag the following blocks to the workspace.<br>
-        What happens when you press the Button B in mBot2?`,
+        image: "srcs/cyberpi/img2/img2_14.png",
+        text: `Let's change the <strong>size</strong> of our car.<br>
+        Drag the block, set 250 as the value, then upload it and try it!`,
         className: ""
     },
     {   //Slide 15
-        video: "srcs/mbot2/img2/img2_15.mp4",
-        text: `One robot was programmed to turn right and the other turn left.<br>
-        Which mBot2 moves clockwise?`,
+        image: "srcs/cyberpi/img2/img2_15.png",
+        text: `Now, let's learn about the axes and directions of the CyberPi screen.<br>
+        The top-left corner is <strong>x</strong> = <strong>0</strong>, <strong>y</strong> = <strong>0</strong>.`,
         className: ""
     },
     {   //Slide 16
-        image: "srcs/mbot2/img2/img2_16.png",
-        text: `Change <strong>RPM</strong> and <strong>secs</strong> to make mBot2 turn right 90 degrees.`,
+        image: "srcs/cyberpi/img2/img2_16.png",
+        text: `If you want an object to be placed more to the right, increase the x value.<br>
+        If you want it lower, increase the y value.`,
         className: ""
     },
     {   //Slide 17
-        image: "srcs/mbot2/img2/img2_17.png",
-        text: `<strong>Challenge:</strong> Move mBot2 in the following shape.`,
+        image: "srcs/cyberpi/img2/img2_17.png",
+        text: `We want the car to start on the <strong>right</strong> and in the <strong>middle</strong>.<br>
+        We will set its position to  x = 128, y = 64.`,
         className: ""
     },
     {   //Slide 18
-        video: "srcs/mbot2/img2/img2_18.mp4",
-        text: `Can mBot2 do more complex movements?`,
+        video: "srcs/cyberpi/img2/img2_1.mp4",
+        text: `Let's give it movement!`,
         className: ""
     },
     {   //Slide 19
-        image: "srcs/mbot2/img2/img2_19.png",
-        text: `Change the name of your project and click Save.<br>
-        There are more options in File menu.`,
+        image: "srcs/cyberpi/img2/img2_19.png",
+        text: `Add the <strong>repeat</strong> block and place the <strong>force rendering</strong> block inside it.`,
+        className: ""
+    },
+    {   //Slide 20
+        image: "srcs/cyberpi/img2/img2_20.png",
+        text: `Add the following block and change its <strong>orientation</strong>.<br>
+        Run the program to see what happens!`,
+        className: ""
+    },
+    {   //Slide 21
+        image: "srcs/cyberpi/img2/img2_21.png",
+        text: `Finally, add a delay so the movement is easier to see.<br>
+        Try it out!`,
+        className: ""
+    },
+    {   //Slide 22
+        video: "srcs/cyberpi/img2/img2_22.mp4",
+        text: `<strong>Challenge</strong>: Move the car below, and add a plane sprite that flies above it, but in the opposite direction.`,
         className: ""
     },
 ];
