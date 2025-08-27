14e08b3ec4d8d2a59dcea1337cbb5215f550c119
vicubyte <vicubyte@gmail.com>
2025-08-27 10:46:15 -0500

    Upload class 8
    Se subió el texto de la clase 8

--- cyberpi/cyberpi_8.js
+++ cyberpi/cyberpi_8.js
@@ -1,116 +1,118 @@
 const slides = [
     {   //Slide 1
-        video: "srcs/mbot2/img8/img8_1.mp4",
-        text: `Learn how to perform <strong>curved movements</strong> with mBot2.`,
+        video: "srcs/cyberpi/img8/img8_1.mp4",
+        text: `Learn how to make a <strong>smart door</strong> with Cyberpi.`,
         className: ""
     },
     {   //Slide 2
-        image: "srcs/mbot2/img8/img8_2.png",
-        text: `Any complex movement can be broken down into many different small curves.`,
+        video: "srcs/cyberpi/img8/img8_2.mp4",
+        text: `How does an automatic door in a mall open when someone approaches?`,
         className: ""
     },
     {   //Slide 3
-        image: "srcs/mbot2/img8/img8_3.png",
-        text: `How can mBot2 perform curved movements?`,
+        video: "srcs/cyberpi/img8/img8_3.mp4",
+        text: `A <strong>PIR sensor</strong> detects movement by measuring infrared radiation from objects.`,
         className: "anim-shake"
     },
     {   //Slide 4
-        image: "srcs/mbot2/img8/img8_4.png",
-        text: `The mBot2 uses two <strong>encoder motors</strong> to perform curved movements!<br>
-        Where else do you find motors?`,
+        image: "srcs/cyberpi/img8/img8_4.png",
+        text: `We’ll create an automatic door system using a <strong>PIR sensor</strong> and a <strong>servo motor</strong>.`,
         className: ""
     },
     {   //Slide 5
-        image: "srcs/mbot2/img8/img8_5.png",
-        text: `Let’s start coding!<br>
-        Create a program that performs different movements!`,
+        image: "srcs/cyberpi/img8/img8_5.png",
+        text: `Let’s build our door!<br>
+        Attach the <strong>servo motor</strong> to the door by inserting the screws from the front and fastening the nuts on the back.`,
         className: ""
     },
     {   //Slide 6
-        image: "srcs/mbot2/img8/img8_6.png",
-        text: `Drag the following blocks. Button A will be the emergency stop button.`,
+        image: "srcs/cyberpi/img8/img8_6.png",
+        text: `Now, attach the door with the servo motor to the <strong>frame</strong>.`,
         className: ""
     },
     {   //Slide 7
-        image: "srcs/mbot2/img8/img8_7.png",
-        text: `We will use this block to move mBot2. It sets different speeds to each encoder motor (EM).`,
+        image: "srcs/cyberpi/img8/img8_7.png",
+        text: `Next, put the <strong>PIR sensor</strong> on top of the frame, with its name facing down.`,
         className: ""
     },
     {   //Slide 8
-        image: "srcs/mbot2/img8/img8_8.png",
-        text: `Drag the following blocks. What will mBot2 do?`,
+        image: "srcs/cyberpi/img8/img8_8.png",
+        text: `Then place the door on the supports.`,
         className: ""
     },
     {   //Slide 9
-        image: "srcs/mbot2/img8/img8_9.png",
-        text: `mBot2 starts turning right! Why is this happening?`,
-        className: "anim-rotate"
+        image: "srcs/cyberpi/img8/img8_9.png",
+        text: `Finally, connect the components like this.`,
+        className: ""
     },
     {   //Slide 10
-        image: "srcs/mbot2/img8/img8_10.png",
-        text: `Encoder motors are assembled in different positions, so they move in opposite directions!`,
+        image: "srcs/cyberpi/img8/img8_10.png",
+        text: `First, we need two new extensions.<br>
+        Click <strong>extension</strong>, and add <strong>PIR Sensor</strong>.`,
         className: ""
     },
     {   //Slide 11
-        image: "srcs/mbot2/img8/img8_11.png",
-        text: `Set EM2 rotate value to -50.<br>
-        Now mBot2 moves forward!`,
+        image: "srcs/cyberpi/img8/img8_11.png",
+        text: `Then, add the <strong>Servo Driver</strong> extension as well.`,
         className: ""
     },
     {   //Slide 12
-        image: "srcs/mbot2/img8/img8_12.png",
-        text: `Set the values to 30 and -30 for slower movement, or to 80 and -80 for faster movement.`,
+        video: "srcs/cyberpi/img8/img8_12.mp4",
+        text: `The servo motor can rotate between 0° and 180°.`,
         className: ""
     },
     {   //Slide 13
-        image: "srcs/mbot2/img8/img8_13.png",
-        text: `Drag the following blocks. Set the values to -50 and 50 for backward movement.`,
+        image: "srcs/cyberpi/img8/img8_13.png",
+        text: `Drag the following blocks.<br>
+        What happens when you press button A?`,
         className: ""
     },
     {   //Slide 14
-        image: "srcs/mbot2/img8/img8_14.png",
-        text: `Set the values to -30 and 30 for slower movement, or to -80 and 80 for faster movement.`,
+        image: "srcs/cyberpi/img8/img8_14.png",
+        text: `Let's modify the angle!<br>
+        What happens when you change it to 90?`,
         className: ""
     },
     {   //Slide 15
-        image: "srcs/mbot2/img8/img8_15.png",
-        text: `Drag the following blocks. Set the values to 50 and 50 for turning right.`,
+        image: "srcs/cyberpi/img8/img8_15.png",
+        text: `The PIR sensor outputs <strong>1</strong> when it detects a person and <strong>0</strong> when it doesn’t.`,
         className: ""
     },
     {   //Slide 16
-        image: "srcs/mbot2/img8/img8_16.png",
-        text: `Set the values to make mBot2 turn left.<br>
-        Look where the center of rotation is located.`,
+        image: "srcs/cyberpi/img8/img8_16.png",
+        text: `Drag the following blocks and test the sensor by moving your hand in front of it.`,
         className: ""
     },
     {   //Slide 17
-        image: "srcs/mbot2/img8/img8_17.png",
-        text: `Drag the following blocks. Set the values to 50 and 0. What will mBot2 do?`,
+        image: "srcs/cyberpi/img8/img8_17.png",
+        text: `Modify your last script.<br>
+        Depending on the result of the PIR sensor block, the if-else block decides what action to take.`,
         className: ""
     },
     {   //Slide 18
-        image: "srcs/mbot2/img8/img8_18.png",
-        text: `Look where the new center of rotation is located. This is a different way to turn!`,
+        image: "srcs/cyberpi/img8/img8_18.png",
+        text: `Analyze this <strong>flowchart</strong>, there is a <strong>conditional</strong> structure and a <strong>loop</strong>.<br>
+        Can you tell what is wrong?`,
         className: ""
     },
     {   //Slide 19
-        image: "srcs/mbot2/img8/img8_19.png",
-        text: `Drag the following blocks. Set the values to 80 and -50. What will mBot2 do?`,
+        image: "srcs/cyberpi/img8/img8_19.png",
+        text: `We have to wait for the person to enter.`,
         className: ""
     },
     {   //Slide 20
-        image: "srcs/mbot2/img8/img8_20.png",
-        text: `The center of rotation changed again! The key is in the difference between the values.`,
+        image: "srcs/cyberpi/img8/img8_20.png",
+        text: `Modify the script with the following blocks, and test it!`,
         className: ""
     },
     {   //Slide 21
-        image: "srcs/mbot2/img8/img8_21.png",
-        text: `Try different values and see how mBot2 moves!`,
+        image: "srcs/cyberpi/img8/img8_21.png",
+        text: `Finally, add LEDs and labels to know the door state.`,
         className: ""
     },
     {   //Slide 22
-        video: "srcs/mbot2/img8/img8_1.mp4",
-        text: `<strong>Challenge:</strong> Program mBot2 to move along this path.`,
+        image: "srcs/cyberpi/img8/img8_1.png",
+        text: `<strong>Challenge:</strong> How would you make the door open from the inside?`,
         className: ""
     },
 ];
