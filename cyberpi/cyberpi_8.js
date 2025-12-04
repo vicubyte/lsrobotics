dffdf07dc3de4967ac94617e4ff8ec9947104762
vicubyte <vicubyte@gmail.com>
2025-12-04 18:27:29 -0500

    new texts cpi8

--- cyberpi/cyberpi_8.js
+++ cyberpi/cyberpi_8.js
@@ -11,7 +11,7 @@ const slides = [
     },
     {   //Slide 3
         image: "srcs/cyberpi/img8/img8_3.png",
-        text: `A <strong>PIR sensor</strong> detects movement by measuring infrared radiation from objects.`,
+        text: `A <strong>PIR sensor</strong> detects movement by measuring <strong>infrared radiation</strong> from objects.`,
         className: "anim-shake"
     },
     {   //Slide 4
@@ -27,12 +27,12 @@ const slides = [
     },
     {   //Slide 6
         image: "srcs/cyberpi/img8/img8_6.png",
-        text: `Now, attach the door with the servo motor to the <strong>frame</strong>.`,
+        text: `Now, attach the door with the servo motor to the frame.`,
         className: ""
     },
     {   //Slide 7
         image: "srcs/cyberpi/img8/img8_7.png",
-        text: `On the other side, put the <strong>PIR sensor</strong> on top of the frame.`,
+        text: `Put the <strong>PIR sensor</strong> on top of the frame and the <strong>Servo driver</strong> on the side.`,
         className: ""
     },
     {   //Slide 8
@@ -42,67 +42,65 @@ const slides = [
     },
     {   //Slide 9
         image: "srcs/cyberpi/img8/img8_9.png",
-        text: `Finally, connect the components like this.`,
+        text: `Finally, connect all the components.`,
         className: ""
     },
     {   //Slide 10
         image: "srcs/cyberpi/img8/img8_10.png",
-        text: `First, we need two new extensions.<br>
-        Click <strong>extension</strong>, and add <strong>PIR Sensor</strong>.`,
+        text: `Your automatic door system should look like this.`,
         className: ""
     },
     {   //Slide 11
         image: "srcs/cyberpi/img8/img8_11.png",
-        text: `Then, add the <strong>Servo Driver</strong> extension as well.`,
+        text: `First, we need two new extensions.<br>
+        Click <strong>extension</strong>, and add <strong>PIR Sensor</strong>.`,
         className: ""
     },
     {   //Slide 12
-        video: "srcs/cyberpi/img8/img8_12.mp4",
-        text: `The servo motor can rotate between 0° and 180°.`,
+        image: "srcs/cyberpi/img8/img8_12.png",
+        text: `Then, add the <strong>Servo Driver</strong> extension as well.`,
         className: ""
     },
     {   //Slide 13
-        image: "srcs/cyberpi/img8/img8_13.png",
-        text: `Drag the following blocks.<br>
-        What happens when you press button A?`,
+        video: "srcs/cyberpi/img8/img8_13.mp4",
+        text: `The servo motor can rotate between 0° and 180°.`,
         className: ""
     },
     {   //Slide 14
         image: "srcs/cyberpi/img8/img8_14.png",
-        text: `Let's modify the angle!<br>
-        What happens when you change it to 90?`,
+        text: `Drag the following blocks.<br>
+        What happens when you press button A?`,
         className: ""
     },
     {   //Slide 15
         image: "srcs/cyberpi/img8/img8_15.png",
-        text: `The PIR sensor outputs <strong>1</strong> when it detects a person and <strong>0</strong> when it doesn’t.`,
+        text: `Let's modify the angle!<br>
+        What happens when you change it to 100?`,
         className: ""
     },
     {   //Slide 16
         image: "srcs/cyberpi/img8/img8_16.png",
-        text: `Drag the following blocks and test the sensor by moving your hand in front of it.`,
+        text: `The PIR sensor outputs <strong>1</strong> when it detects a person and <strong>0</strong> when it doesn’t.`,
         className: ""
     },
     {   //Slide 17
         image: "srcs/cyberpi/img8/img8_17.png",
-        text: `Modify your last script.<br>
-        Depending on the result of the PIR sensor block, the if-else block decides what action to take.`,
+        text: `Drag the following blocks and test the sensor by moving your hand in front of it.`,
         className: ""
     },
     {   //Slide 18
         image: "srcs/cyberpi/img8/img8_18.png",
-        text: `Analyze this <strong>flowchart</strong>, there is a <strong>conditional</strong> structure and a <strong>loop</strong>.<br>
-        Can you tell what is wrong?`,
+        text: `Depending on the result of the <strong>PIR sensor block</strong>, the if-else block decides what action to take.`,
         className: ""
     },
     {   //Slide 19
         image: "srcs/cyberpi/img8/img8_19.png",
-        text: `We have to wait for the person to enter.`,
+        text: `Analyze this <strong>flowchart</strong>, there is a <strong>conditional</strong> structure and a <strong>loop</strong>.`,
         className: ""
     },
     {   //Slide 20
         image: "srcs/cyberpi/img8/img8_20.png",
-        text: `Modify the script with the following blocks, and test it!`,
+        text: `Drag the following blocks and test this script!`,
         className: ""
     },
     {   //Slide 21
